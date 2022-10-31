d880b7b9825eb49a9ebc8d44e607eeb4f7a7c899
SecondDim <8318845+SecondDim@users.noreply.github.com>
2022-10-31 09:43:29 +0800

    Update aws-ubuntu2204-init.sh

--- aws-ubuntu2204-init.sh
+++ aws-ubuntu2204-init.sh
@@ -2,18 +2,26 @@
 
 set -e
 
-# init
+echo "=================================================="
+echo "========== init update ==========================="
+echo "=================================================="
+
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 
 echo "=================================================="
+echo "========== download file ========================="
+echo "=================================================="
 
-# download file
 curl -s https://raw.githubusercontent.com/SecondDim/script/main/.bash_aliases > ~/.bash_aliases
 
+echo "=================================================="
+echo "========== set aws ntp ==========================="
 echo "=================================================="
 
 # set aws ntp
 sudo apt install chrony
+
+set -x
 
 match="#\ See\ http:\/\/www.pool.ntp.org\/join.html for more information."
 insert="server\ 169.254.169.123\ prefer\ iburst\ minpoll\ 4\ maxpoll\ 4"
@@ -23,4 +31,6 @@ sudo sed -i "s/$match/$match\n$insert/" $file
 
 sudo /etc/init.d/chrony restart
 
-echo "=================================================="
+set +x
+
+echo "========== [script done] ========================="
