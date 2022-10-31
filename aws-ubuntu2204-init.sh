3b490f0f373866e43fa1e94540f2b65820fb8d07
SecondDim <8318845+SecondDim@users.noreply.github.com>
2022-10-31 09:15:10 +0800

    Update aws-ubuntu2204-init.sh

--- aws-ubuntu2204-init.sh
+++ aws-ubuntu2204-init.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sudo apt update && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 
 curl -s https://raw.githubusercontent.com/SecondDim/script/main/.bash_aliases > ~/.bash_aliases
